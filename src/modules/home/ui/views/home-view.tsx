85fe7a9bb3898729e8be8ee17bc749d7e964a03f
Sameer <sameersingh192004@gmail.com>
2025-07-21 02:55:07 +0530

    09: Agents initail setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
-
 export const HomeView = () => {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.greeting.queryOptions({ name: "Sameer" }));
   return (
     <div>
-      {data?.message}
+      Home View
     </div>
   )
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,31 +1,7 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { createTRPCRouter } from '../init';
+import { agentsRouter } from '@/modules/agents/server/procedures';
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      }),
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
-
-  // Add other routers here
-  // add a basic example router
-  greeting: baseProcedure
-    .input(
-      z.object({
-        name: z.string(),
-      }),
-    )
-    .query((opts) => {
-      return {
-        message: `Hello, ${opts.input.name}!`,
-      };
-    }),
+  agents: agentsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
